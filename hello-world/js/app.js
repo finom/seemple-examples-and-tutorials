079177951a1b47ae297144794f3f87df65e82db1
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-10-03 00:18:31 +0300

    typo fix

--- hello-world/js/app.js
+++ hello-world/js/app.js
@@ -15,7 +15,7 @@ class Application extends Matreshka {
         // if the property "х" has changed,
         // inform about it in the console
         this.on('change:x', () =>
-            console.log(`x изменен на "${this.x}"`));
+            console.log(`is changed to "${this.x}"`));
     }
 }
 
